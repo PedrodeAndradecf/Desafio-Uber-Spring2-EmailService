e19edffa1e22e18d4997d88367475f714622078e
PedrodeAndradecf <>
2025-06-09 13:31:26 -0300

    feat: adicionado o pomxml e as dependencias

---
+++ src/test/java/com/Uber/desafio_spring_boot/DesafioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Uber.desafio_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
